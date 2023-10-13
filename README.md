9630746ad9327ba60133fc6a48003a3322893956
Mojtaba Imani <sm.imani@gmail.com>
2023-10-13 10:36:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# idp-kubernetes
+
+idp-kubernetes is an internal developer platform that installs core apps into a kubernetes cluster and makes it ready to deploy custom services. Then developers can use this platform to deploy their services to different environments.
+
+## Use case
+
+Assume that there are multiple business or developer teams called blue, red, green, etc. There is one platform team that provides the kubernetes platform and platform tools and automation so that developer teams can focus on their own business applications and development.
+
+Platform team creates a namespace for each team and each team can deploy their services to only their own namespace.
+
+There are two Kubernetes clusters one for dev/test/acc (dta) and another one dedicated to the production (prd) environment. dev/test/acc in dta cluster are separated in different namespaces. They use one cluster for those environment to save cost. otherwise they can have separate cluster for each environment.
+
+## Values
+
+You can define a separate repository for values. If you do so, you can update the main platform source without affecting your values.
+
+## Installation
+
+You need to have kubectl and helm commands installed in your local environment then use these commands to install and configure argocd. Then argocd will pull the code from the repository and deploy apps to the cluster.
+
+```bash
+cicd/scripts/install.sh <Kubernetes context> <git repository token> <environment name>
+```
+
+example command for dta and prd environments are:
+
+dta:
+```
+cicd/scripts/install.sh minikube-dta <token> dta
+```
+
+prd:
+```
+cicd/scripts/install.sh minikube-prd <token> prd
+```
---
+++ cicd/scripts/install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CONTEXT=${1-}               # example: minikube-dta
+ENVIRONMENT=${2-dta}        # example: dta or prd (defaults to dta)
+REPOSITORY_TOKEN=${3-}      # example: 1234567890qpoieraksjdhzxcbv
+
+# context MUST be set to connect to the k8s cluster
+if [ -z "${CONTEXT}" ]
+then
+  echo 1>&2 CONTEXT is undefined
+  exit 2
+fi
+
+
+# create namespace
+NAMESPACE=argocd
+
+kubectl create namespace $NAMESPACE --context $CONTEXT --dry-run=client -o yaml | kubectl apply --context $CONTEXT -f -
+
+# create secret for repository if token is set for it
+if [ -n "${REPOSITORY_TOKEN}" ]
+then
+kubectl create secret generic argocd-repo-platform --context $CONTEXT -n $NAMESPACE \
+  --from-literal=name=idp \
+  --from-literal=username=x \
+  --from-literal=password=$REPOSITORY_TOKEN \
+  --from-literal=url=https://github.com/mojtabaimani/idp-kubernetes.git \
+  --from-literal=type=git \
+  --dry-run=client -o yaml | kubectl apply --context $CONTEXT -n $NAMESPACE -f -
+kubectl label secret argocd-repo-platform argocd.argoproj.io/secret-type=repository --context $CONTEXT -n $NAMESPACE
+
+kubectl create secret generic argocd-repo-green-services --context $CONTEXT -n $NAMESPACE \
+  --from-literal=name=green-services \
+  --from-literal=username=x \
+  --from-literal=password=$REPOSITORY_TOKEN \
+  --from-literal=url=https://github.com/mojtabaimani/green-services.git \
+  --from-literal=type=git \
+  --dry-run=client -o yaml | kubectl apply --context $CONTEXT -n $NAMESPACE -f -
+kubectl label secret argocd-repo-green-services argocd.argoproj.io/secret-type=repository --context $CONTEXT -n $NAMESPACE
+fi
+
+# install argocd using helm
+PROJECT_NAME=platform-$ENVIRONMENT
+VALUES_FILE=values/argocd/values-$ENVIRONMENT.yaml
+# helm repo add argocd https://argoproj.github.io/argo-helm
+# helm repo update
+helm upgrade --install --kube-context $CONTEXT -n $NAMESPACE -f $VALUES_FILE argocd argocd/argo-cd --version 5.42.2 --wait
+
+
+# add project to the argocd server using yaml file
+cat <<EOF | kubectl apply --context $CONTEXT -n $NAMESPACE -f -
+apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: $PROJECT_NAME
+  namespace: $NAMESPACE
+  # Finalizer that ensures that project is not deleted until it is not referenced by any application
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+spec:
+  sourceRepos:
+  - '*'
+  destinations:
+  - name: '*'
+    namespace: '*'
+    server: https://kubernetes.default.svc
+  clusterResourceWhitelist:
+  - group: '*'
+    kind: '*'
+  namespaceResourceWhitelist:
+  - group: '*'
+    kind: '*'
+EOF
+
+# add app of the apps to the argocd server using yaml file
+kubectl apply --context $CONTEXT -n $NAMESPACE -f cicd/argocd/app-of-apps-$ENVIRONMENT.yaml
